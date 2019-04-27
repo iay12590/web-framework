c1a1da152bdde3e3d10f6acb84397225ca702586
iay <897291546@qq.com>
2019-04-27 22:46:51 +0800

    init

---
+++ web-framework/src/main/java/com/warden/webframework/WebFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.warden.webframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFrameworkApplication.class, args);
+	}
+
+}
